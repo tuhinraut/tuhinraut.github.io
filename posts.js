e6135e8000e5fa26886438ba2f6f661fa7bd81e4
Tuhin Kumar Raut <tuhinraut@gmail.com>
2025-03-19 04:21:28 +0530

    How close are we not working

--- posts.js
+++ posts.js
@@ -13,7 +13,7 @@ const postsData = [
     "title": "... to building a fully organic computer?",
     "date": "April 10, 2025",
     "image": "organic_computer.png",
-    "content": "<i>You've seen those sci-fi scenes where a human brain pilots a spaceship, right? While that might've been a physical depiction of AI, the concept of creating a truly \"organic computer\" remains fascinatingly within reach.</i>",
+    "content": "<i>You've seen those sci-fi scenes where a human brain pilots a spaceship, right? While that might've been a physical depiction of AI, the concept of creating a truly organic computer remains fascinatingly within reach.</i>",
     "link": "#",
     "status": "coming soon", // This post is coming soon
     "tags": ["technology", "organic computing", "biology"]
@@ -22,7 +22,7 @@ const postsData = [
     "title": "... to solving global warming with biology?",
     "date": "May 5, 2025",
     "image": "global_warming.png",
-    "content": "<i>Aah.. the age old, but very important nonetheless, topic of \"Global Warming\". It needs to be solved, but can it be done by using biology?</i>",
+    "content": "<i>Aah.. the age old, but very important nonetheless, topic of Global Warming. It needs to be solved, but can it be done by using biology?</i>",
     "link": "#",
     "status": "coming soon", // This post is coming soon
     "tags": ["climate change", "biology", "environment"]
