74d180f4d83631bef7888148a001b5cfac80d8d4
Tuhin Kumar Raut <tuhinraut@gmail.com>
2025-08-11 20:43:07 +0530

    11 aug

--- content/ideas.js
+++ content/ideas.js
@@ -1,30 +1,30 @@
-const biologyData = [
+const ideasData = [
   {
-    "title": "Dogma as Grandma",
-    "date": "Aug 25, 2025",
+    "title": "Bionic Arm",
+    "date": "Aug 15, 2025",
     "image": "./pictures/dogma-grandma.png",
     "content": "<i>We all might be familar with the central dogma of life, but I just wanted to start with a very light topic and a very tasty one in that.</i>",
     "link": "#",
     "status": "posted",
-    "tags": [ "genetics"]
+    "tags": ["biology", "analogy", "genetics"]
   },
   {
-    "title": "The Cell as a Factory",
+    "title": "Capsule for Cancer",
     "date": "TBD",
     "image": "./pictures/cell-factory.png",
     "content": "<i>Your cells house nature's ultimate manufacturing marvel. Microscopic assembly lines operate with perfect efficiency while self-replicating and repairing autonomously. This factory has been perfecting operations for billions of years before humans dreamed of assembly lines.</i>",
     "link": "#",
     "status": "coming soon",
-    "tags": ["cell biology"]
+    "tags": ["biology", "analogy", "cell biology"]
   },
 
   {
-    "title": "Immune System as Military",
+    "title": "The Second Brain",
     "date": "TBD",
     "image": "./pictures/immune-military.png",
     "content": "<i>Forget US Military—your body houses the most sophisticated military force in known existence. An intricate network of specialized cells patrols your bloodstream. This army wages constant warfare against enemies, distinguishes friend from foe, and adapts its weaponry in real-time.</i>",
     "link": "#",
     "status": "coming soon",
-    "tags": ["immunology"]
+    "tags": ["biology", "analogy", "immunology"]
   }
 ];
--- content/posts.js
+++ content/posts.js
@@ -3,35 +3,35 @@
 const postsData = [
     {
         "title": "... to making our own organism from scratch?",
-        "date": "March 25, 2025",
+        "date": "Sept 5, 2025",
         "author": "Jonathan Doe",
         "image": "./pictures/platypus.png",
         "content": "Imagine a world where we could custom-design organisms to our exact specifications. It seems like a fever dream, but the reality of how close we are might shock you.",
         "mdFile": "post1.md",
         "link": "blog.html?id=0",
         "status": "posted",
-        "tags": ["biology", "synthetic biology", "genetics"]
+        "tags": ["synthetic biology", "aliens"]
     },
     {
         "title": "... to building a fully organic computer?",
-        "date": "April 10, 2025",
+        "date": "TBD",
         "author": "Jonathan Doe",
         "image": "./pictures/organic_computer.png",
         "content": "You've seen those sci-fi scenes where a human brain pilots a spaceship, right? While that might've been a physical depiction of AI, the concept of creating a truly \"organic computer\" remains fascinatingly within reach.",
         "mdFile": "post2.md",
         "link": "blog.html?id=1",
         "status": "coming soon",
-        "tags": ["technology", "organic computing", "biology"]
+        "tags": ["organic computers", ""]
     },
     {
         "title": "... to solving global warming with biology?",
-        "date": "May 5, 2025",
+        "date": "TBD",
         "author": "Jonathan Doe",
         "image": "./pictures/global_warming.png",
         "content": "Aah.. the age old, but very important nonetheless, topic of \"Global Warming\". It needs to be solved, but can it be done by using biology?",
         "mdFile": "post3.md",
         "link": "blog.html?id=2",
         "status": "coming soon",
-        "tags": ["climate change", "biology", "environment"]
+        "tags": ["climate change", "penguins"]
     }
 ];
